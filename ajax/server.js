294fbd4773e8b0951bed3d8205f108615eeca2cd
lucssant <lucaslindo832@gmail.com>
2021-09-21 22:37:43 -0300

    ajax fim

--- ajax/server.js
+++ ajax/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true}))
+/app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
@@ -28,6 +28,22 @@ app.post('/upload', (req,res) => {
 
     res.end('Concluido com sucesso.')
   }) 
+})
+
+app.post('/formulario', (req, res) => {
+    res.send(
+      {
+        ...req.body,
+        id: 1,
+      }
+    )
+})
+
+app.get('/parOuImpar', (req,res) => {
+  const par = parseInt(req.query.numero) % 2 === 0
+  res.send({
+    resultado: par ? 'par' : 'impar'
+  })
 })
 
 
